06aee7ef0f159879de07c88d53e0e220e4f89463
aman-gupta-2006 <amankvdelhi@gmail.com>
2025-06-04 02:09:47 +0530

    change made

--- project2/index.js
+++ project2/index.js
@@ -7,19 +7,21 @@ form.addEventListener("submit", (event) => {
     const buttonClick = document.querySelector("button");
     const result = document.querySelector("#result");
 
-    buttonClick.addEventListener("click", ()=>{
+    buttonClick.addEventListener("click", () => {
         const heightInputSquare = Math.pow(heightInput, 2);
-        
+
         const BMI = weightInput / heightInputSquare;
 
         let status = "";
-        if(BMI < 18.6){
+
+        if (BMI < 18.6) {
             status = "Under Weight"
-        } else if(BMI >= 18.6 && BMI <= 24.9){
+        } else if (BMI >= 18.6 && BMI <= 24.9) {
             status = "Normal Weight"
-        } else{
+        } else {
             status = "Over Weight"
         }
+
         result.innerText = `Your BMI is, ${BMI} and you fall in ${status} category`;
     });
 });
